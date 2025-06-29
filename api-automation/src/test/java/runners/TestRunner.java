ca8014a47477350fa27693dfc5a6e5dff4cfa9a5
giitanjalim@gmail.com <giitanjalim@gmail.com>
2025-06-29 16:45:18 +0530

    testrunner

--- api-automation/src/test/java/runners/TestRunner.java
+++ api-automation/src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/java/features",
         glue = {"stepdefs"},
         plugin = {
                 "pretty",
